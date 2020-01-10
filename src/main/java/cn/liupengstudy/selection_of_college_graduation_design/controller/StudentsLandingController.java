33978fcdf857eec80a04eb06f2f9e6eeee44c594
liupeng <37412816+liupengzhouyi@users.noreply.github.com>
2020-01-10 23:39:14 +0800

    add student landing information V 1.0.4

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/controller/StudentsLandingController.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/controller/StudentsLandingController.java
@@ -1,5 +1,6 @@
 package cn.liupengstudy.selection_of_college_graduation_design.controller;
 
+import cn.liupengstudy.selection_of_college_graduation_design.pojo.ReturnInformation;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.StudentsLandingTable;
 import cn.liupengstudy.selection_of_college_graduation_design.service.impl.StudentsLandingTableServiceImpl;
 import io.swagger.annotations.Api;
@@ -24,26 +25,56 @@ import org.springframework.web.bind.annotation.RestController;
 @Api(tags = "学生登陆控制器", description = "提供学生登陆控制器的增删改查方法")
 public class StudentsLandingController {
 
+    // service
     @Autowired
     private StudentsLandingTableServiceImpl studentsLandingTableServiceImpl;
 
+    /*
+     * @Title getStudentsLandingTableServiceImpl
+     * @Description get students landing service
+     * @Param []
+     * @return cn.liupengstudy.selection_of_college_graduation_design.service.impl.StudentsLandingTableServiceImpl
+     * @Date 1/10/2020 11:27 PM
+     * @Author liupeng
+     **/
     public StudentsLandingTableServiceImpl getStudentsLandingTableServiceImpl() {
         return studentsLandingTableServiceImpl;
     }
 
+    /*
+     * @Title setStudentsLandingTableServiceImpl
+     * @Description set students landing service
+     * @Param [studentsLandingTableServiceImpl]
+     * @return void
+     * @Date 1/10/2020 11:28 PM
+     * @Author liupeng
+     **/
     public void setStudentsLandingTableServiceImpl(StudentsLandingTableServiceImpl studentsLandingTableServiceImpl) {
         this.studentsLandingTableServiceImpl = studentsLandingTableServiceImpl;
     }
 
+    /*
+     * @Title addStudent
+     * @Description add student landing information
+     * @Param [studentsLandingTable]
+     * @return cn.liupengstudy.selection_of_college_graduation_design.pojo.ReturnInformation
+     * @Date 1/10/2020 11:37 PM
+     * @Author liupeng
+     **/
     @ApiOperation(value = "添加学生登陆信息")
     @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
-    public String addStudent(@RequestBody StudentsLandingTable studentsLandingTable) {
+    public ReturnInformation addStudent(@RequestBody StudentsLandingTable studentsLandingTable) {
+        ReturnInformation returnInformation = new ReturnInformation();
+        returnInformation.setWhatYourDo("add student landing information");
         int k = this.getStudentsLandingTableServiceImpl().insert(studentsLandingTable);
         if (k == 1) {
-            return "success";
+            returnInformation.setWhy("add success");
+            returnInformation.setKey(true);
         } else {
-            return "error";
+            returnInformation.setWhy("add error");
+            returnInformation.setKey(false);
         }
+        return returnInformation;
     }
 
 
