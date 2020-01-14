b5189779fc7b853a8ff60a9b1fc2e155b34e9f8e
liupeng <liupeng.0@outlook.com>
2020-01-14 22:35:31 +0800

    update ManagersDetailsTableMapper.java add interface findByManagerID V 1.8.2

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/ManagersDetailsTableMapper.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/ManagersDetailsTableMapper.java
@@ -1,7 +1,12 @@
 package cn.liupengstudy.selection_of_college_graduation_design.mapper;
 
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.ManagersDetailsTable;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
+@Mapper
 public interface ManagersDetailsTableMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +19,6 @@ public interface ManagersDetailsTableMapper {
     int updateByPrimaryKeySelective(ManagersDetailsTable record);
 
     int updateByPrimaryKey(ManagersDetailsTable record);
+
+    List<ManagersDetailsTable> findByManagerID(@Param("managerID") String managerID);
 }
