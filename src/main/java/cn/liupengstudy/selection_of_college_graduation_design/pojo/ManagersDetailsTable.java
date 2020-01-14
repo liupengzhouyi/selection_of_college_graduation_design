c2983aaab5ae68710926c02922491989e16f8922
liupeng <liupeng.0@outlook.com>
2020-01-14 22:28:42 +0800

    Manager details information pojo add comment V 1.8.1

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/pojo/ManagersDetailsTable.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/pojo/ManagersDetailsTable.java
@@ -1,12 +1,21 @@
 package cn.liupengstudy.selection_of_college_graduation_design.pojo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "管理员实体")
 public class ManagersDetailsTable {
+
+    @ApiModelProperty(value = "自增ID")
     private Integer id;
 
+    @ApiModelProperty(value = "管理员ID")
     private Integer managerid;
 
+    @ApiModelProperty(value = "联系方式")
     private String phonenumber;
 
+    @ApiModelProperty(value = "姓名")
     private String name;
 
     public Integer getId() {
