36055a4289b858ce5596e122881b79c64f3d1483
liupeng <liupeng.0@outlook.com>
2020-01-14 21:33:55 +0800

    add function which named findByTeacherID in mapper and mapper.xml V 1.7.1

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/TeachersDetailsTableMapper.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/TeachersDetailsTableMapper.java
@@ -1,7 +1,12 @@
 package cn.liupengstudy.selection_of_college_graduation_design.mapper;
 
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.TeachersDetailsTable;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
+@Mapper
 public interface TeachersDetailsTableMapper {
     int deleteByPrimaryKey(Integer id);
 
@@ -14,4 +19,6 @@ public interface TeachersDetailsTableMapper {
     int updateByPrimaryKeySelective(TeachersDetailsTable record);
 
     int updateByPrimaryKey(TeachersDetailsTable record);
+
+    List<TeachersDetailsTable> findByTeacherID(@Param("teacherID")String teacherID);
 }
