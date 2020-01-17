eb005fff63b321abc048dd7dd6a935f6fe6872b5
liupeng <liupeng.0@outlook.com>
2020-01-17 23:53:20 +0800

    implements GroupNumberTableService V 1.13.6

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/service/impl/GroupNumberTableServiceImpl.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/service/impl/GroupNumberTableServiceImpl.java
@@ -1,7 +1,11 @@
 package cn.liupengstudy.selection_of_college_graduation_design.service.impl;
 
+import cn.liupengstudy.selection_of_college_graduation_design.mapper.GroupNumberTableMapper;
+import cn.liupengstudy.selection_of_college_graduation_design.mapper.GroupRelationshipTableMapper;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.GroupNumberTable;
 import cn.liupengstudy.selection_of_college_graduation_design.service.GroupNumberTableService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
@@ -13,44 +17,57 @@ import java.util.List;
  * @创建时间 2020/1/17 - 11:48 下午
  * @修改人和其它信息
  */
+@Service
 public class GroupNumberTableServiceImpl implements GroupNumberTableService {
+
+    @Autowired
+    private GroupNumberTableMapper groupNumberTableMapper;
+
+    public GroupNumberTableMapper getGroupNumberTableMapper() {
+        return groupNumberTableMapper;
+    }
+
+    public void setGroupNumberTableMapper(GroupNumberTableMapper groupNumberTableMapper) {
+        this.groupNumberTableMapper = groupNumberTableMapper;
+    }
+
     @Override
     public int deleteByPrimaryKey(Integer id) {
-        return 0;
+        return this.getGroupNumberTableMapper().deleteByPrimaryKey(id);
     }
 
     @Override
     public int insert(GroupNumberTable record) {
-        return 0;
+        return this.getGroupNumberTableMapper().insert(record);
     }
 
     @Override
     public int insertSelective(GroupNumberTable record) {
-        return 0;
+        return this.getGroupNumberTableMapper().insertSelective(record);
     }
 
     @Override
     public GroupNumberTable selectByPrimaryKey(Integer id) {
-        return null;
+        return this.getGroupNumberTableMapper().selectByPrimaryKey(id);
     }
 
     @Override
     public int updateByPrimaryKeySelective(GroupNumberTable record) {
-        return 0;
+        return this.getGroupNumberTableMapper().updateByPrimaryKeySelective(record);
     }
 
     @Override
     public int updateByPrimaryKey(GroupNumberTable record) {
-        return 0;
+        return this.getGroupNumberTableMapper().updateByPrimaryKey(record);
     }
 
     @Override
     public List<GroupNumberTable> findAllGroupNumber(Integer integer) {
-        return null;
+        return this.getGroupNumberTableMapper().findAllGroupNumber(integer);
     }
 
     @Override
     public List<GroupNumberTable> findStudent(String studentID) {
-        return null;
+        return this.getGroupNumberTableMapper().findStudent(studentID);
     }
 }
