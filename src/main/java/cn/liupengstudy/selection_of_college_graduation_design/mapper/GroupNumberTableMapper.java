abf29a54dfb007b992235498231698c756c7225a
liupeng <liupeng.0@outlook.com>
2020-01-17 23:44:20 +0800

    update GroupNumberTableMapper V 1.13.1

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/GroupNumberTableMapper.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/mapper/GroupNumberTableMapper.java
@@ -1,8 +1,14 @@
 package cn.liupengstudy.selection_of_college_graduation_design.mapper;
 
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.GroupNumberTable;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
+import java.util.List;
+
+@Mapper
 public interface GroupNumberTableMapper {
+
     int deleteByPrimaryKey(Integer id);
 
     int insert(GroupNumberTable record);
@@ -14,4 +20,8 @@ public interface GroupNumberTableMapper {
     int updateByPrimaryKeySelective(GroupNumberTable record);
 
     int updateByPrimaryKey(GroupNumberTable record);
+
+    List<GroupNumberTable> findAllGroupNumber(@Param("groupID")Integer integer);
+
+    List<GroupNumberTable> findStudent(@Param("studentID") String studentID);
 }
