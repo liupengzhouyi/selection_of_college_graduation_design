60809e999e21494027a516894e91d689e69f4e0a
liupeng <liupeng.0@outlook.com>
2020-01-17 23:49:35 +0800

    GroupNumberTableServiceImpl implements GroupNumberTableService interface V 1.13.5

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/service/impl/GroupNumberTableServiceImpl.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/service/impl/GroupNumberTableServiceImpl.java
@@ -1,5 +1,10 @@
 package cn.liupengstudy.selection_of_college_graduation_design.service.impl;
 
+import cn.liupengstudy.selection_of_college_graduation_design.pojo.GroupNumberTable;
+import cn.liupengstudy.selection_of_college_graduation_design.service.GroupNumberTableService;
+
+import java.util.List;
+
 /**
  * @文件名 cn.liupengstudy.selection_of_college_graduation_design.service.impl
  * @描述
@@ -8,5 +13,44 @@ package cn.liupengstudy.selection_of_college_graduation_design.service.impl;
  * @创建时间 2020/1/17 - 11:48 下午
  * @修改人和其它信息
  */
-public class GroupNumberTableServiceImpl {
+public class GroupNumberTableServiceImpl implements GroupNumberTableService {
+    @Override
+    public int deleteByPrimaryKey(Integer id) {
+        return 0;
+    }
+
+    @Override
+    public int insert(GroupNumberTable record) {
+        return 0;
+    }
+
+    @Override
+    public int insertSelective(GroupNumberTable record) {
+        return 0;
+    }
+
+    @Override
+    public GroupNumberTable selectByPrimaryKey(Integer id) {
+        return null;
+    }
+
+    @Override
+    public int updateByPrimaryKeySelective(GroupNumberTable record) {
+        return 0;
+    }
+
+    @Override
+    public int updateByPrimaryKey(GroupNumberTable record) {
+        return 0;
+    }
+
+    @Override
+    public List<GroupNumberTable> findAllGroupNumber(Integer integer) {
+        return null;
+    }
+
+    @Override
+    public List<GroupNumberTable> findStudent(String studentID) {
+        return null;
+    }
 }
