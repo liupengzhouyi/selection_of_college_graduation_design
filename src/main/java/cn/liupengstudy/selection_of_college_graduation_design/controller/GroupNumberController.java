c410b1611ee506b2fc749dc3d40c65eef79a9512
liupeng <liupeng.0@outlook.com>
2020-01-17 23:54:30 +0800

    create GroupNumberController V 1.13.7

---
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/controller/GroupNumberController.java
@@ -0,0 +1,12 @@
+package cn.liupengstudy.selection_of_college_graduation_design.controller;
+
+/**
+ * @文件名 cn.liupengstudy.selection_of_college_graduation_design.controller
+ * @描述
+ * @创建人 liupeng
+ * @作者联系方式 LIUPENG.0@outlook.com
+ * @创建时间 2020/1/17 - 11:54 下午
+ * @修改人和其它信息
+ */
+public class GroupNumberController {
+}
