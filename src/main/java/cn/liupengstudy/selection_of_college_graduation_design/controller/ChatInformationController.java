1f1a54bdc972bcc14cf8cf112fe72a7d9675a988
liupeng <liupeng.0@outlook.com>
2020-02-19 01:16:54 +0800

    all get chat information version tow, add tow List 2.0.12

--- src/main/java/cn/liupengstudy/selection_of_college_graduation_design/controller/ChatInformationController.java
+++ src/main/java/cn/liupengstudy/selection_of_college_graduation_design/controller/ChatInformationController.java
@@ -1,11 +1,13 @@
 package cn.liupengstudy.selection_of_college_graduation_design.controller;
 
+import cn.liupengstudy.selection_of_college_graduation_design.pojo.CharInformationVersionII;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.ChatInformationTable;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.tools.dataType.IntegerType;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.tools.getNowTime.GetNowDate;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.tools.getNowTime.GetNowTime;
 import cn.liupengstudy.selection_of_college_graduation_design.pojo.tools.returnType.ReturnInformation;
 import cn.liupengstudy.selection_of_college_graduation_design.service.impl.ChatInformationTableServiceImpl;
+import cn.liupengstudy.selection_of_college_graduation_design.service.impl.GetChatInformation;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import net.bytebuddy.asm.Advice;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Date;
+import java.sql.SQLException;
 import java.sql.Time;
 import java.util.List;
 
@@ -75,6 +78,24 @@ public class ChatInformationController {
         returnInformation.setWhatYourDo("add chat information");
         List<ChatInformationTable> list = null;
         list = this.getChatInformationTableServiceImpl().getGroupChatInformation(integerType.getInteger());
+        if (list.size() != 0) {
+            returnInformation.setKey(true);
+            returnInformation.setWhy("find success");
+            returnInformation.setReturnObject(list);
+        } else {
+            returnInformation.setKey(false);
+            returnInformation.setWhy("find error");
+        }
+        return returnInformation;
+    }
+
+    @ApiOperation(value = "获取聊天信息二代")
+    @RequestMapping(value = "/getGroupInformationII", method = RequestMethod.POST, produces = "application/json;charset=UTF-8")
+    public ReturnInformation getGroupChatInformationVersionII(@RequestBody IntegerType integerType) throws SQLException {
+        ReturnInformation returnInformation = new ReturnInformation();
+        returnInformation.setWhatYourDo("add chat information");
+        List<CharInformationVersionII> list = null;
+        list = new GetChatInformation(integerType.getInteger()).getList();
         if (list.size() != 0) {
             returnInformation.setKey(true);
             returnInformation.setWhy("find success");
